1880bee8a913a8638e47dafc2a801ef2ffc3bc5f
aldahir sealtiel alvarez villegas <alda_dsr@hotmail.com>
2023-03-31 19:47:20 -0600

    Se agrego el comportamiento de toda la pantalla principal del score

--- app/src/main/java/com/example/basketballscore/MainActivity.java
+++ app/src/main/java/com/example/basketballscore/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.basketballscore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     Integer localScore = 0;
     Integer visitorScore =0;
+
+    public static final String LOCAL_SCORE_KEY = "SCORE_LOCAL_PLAYER";
+    public static final String VISITOR_SCORE_KEY = "SCORE_VISITOR_PLAYER";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +28,76 @@ public class MainActivity extends AppCompatActivity {
             restarLocalScore();
         });
         binding.addOneLocalScoreBtn.setOnClickListener(v->{
-            sumarMarcadorLocalScore(1);
+            sumarLocalScore(1);
         });
+        binding.addTwoLocalScoreBtn.setOnClickListener( v->{
+            sumarLocalScore(2);
+        });
+        binding.addOneVisitorScoreBtn.setOnClickListener(v->{
+            sumarVisitorScore(1);
+        });
+        binding.addTwoVisitorScoreBtn.setOnClickListener(v->{
+            sumarVisitorScore(2);
+        });
+        binding.substractVisitorScoreBtn.setOnClickListener(v->{
+            restarVisitorScore();
+        });
+        binding.resetScoreBtn.setOnClickListener(v->{
+            resetScores();
+        });
+        binding.detallesBtn.setOnClickListener(v->{
+            abrirPantallaDetallesPartido();
+        });
+
     }
 
-    private void sumarMarcadorLocalScore(int valorASumar)
+    private void abrirPantallaDetallesPartido()
+    {
+        Intent intent = new Intent(this, ResultadosPartido.class);
+        intent.putExtra(LOCAL_SCORE_KEY, localScore);
+        intent.putExtra(VISITOR_SCORE_KEY, visitorScore);
+        startActivity(intent);
+    }
+    private void resetScores()
+    {
+        visitorScore = 0;
+        localScore = 0;
+        binding.VisitanteScoreText.setText(visitorScore.toString());
+        binding.LocalScoreText.setText(localScore.toString());
+    }
+    private void restarVisitorScore()
+    {
+        if(visitorScore - 1 >= 0)
+        {
+            visitorScore --;
+            binding.VisitanteScoreText.setText(visitorScore.toString());
+        }
+    }
+
+    private void sumarVisitorScore(int valorASumar)
+    {
+        if(valorASumar == 1)
+        {
+            visitorScore ++;
+            binding.VisitanteScoreText.setText(visitorScore.toString());
+        }
+        if(valorASumar == 2)
+        {
+            visitorScore +=2;
+            binding.VisitanteScoreText.setText(visitorScore.toString());
+        }
+    }
+
+    private void sumarLocalScore(int valorASumar)
     {
         if(valorASumar == 1)
         {
             localScore ++;
+            binding.LocalScoreText.setText(localScore.toString());
+        }
+        if(valorASumar == 2)
+        {
+            localScore +=2;
             binding.LocalScoreText.setText(localScore.toString());
         }
     }
---
+++ app/src/main/java/com/example/basketballscore/ResultadosPartido.java
@@ -0,0 +1,14 @@
+package com.example.basketballscore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadosPartido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados_partido);
+    }
+}
