dc730627bd6e85eff72e00e5f1743f2975085111
aldahir sealtiel alvarez villegas <alda_dsr@hotmail.com>
2023-04-01 13:53:56 -0600

    finalización del ejercicio del score

--- app/src/main/java/com/example/basketballscore/ResultadosPartido.java
+++ app/src/main/java/com/example/basketballscore/ResultadosPartido.java
@@ -1,14 +1,48 @@
 package com.example.basketballscore;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.basketballscore.databinding.ActivityResultadosPartidoBinding;
 
 public class ResultadosPartido extends AppCompatActivity {
 
+    ActivityResultadosPartidoBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resultados_partido);
+        binding = ActivityResultadosPartidoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent datosDeMainActivity = getIntent();
+        Bundle extras = datosDeMainActivity.getExtras();
+        Integer localScore = extras.getInt(MainActivity.LOCAL_SCORE_KEY);
+        Integer visitorScore = extras.getInt(MainActivity.VISITOR_SCORE_KEY);
+        binding.localScore.setText(localScore.toString());
+        binding.VisitorScore.setText(visitorScore.toString());
+        String res = validateResult(localScore, visitorScore);
+        binding.resultadoPartido.setText(res);
+    }
+
+    public String validateResult(Integer local, Integer visitor){
+        String ans = "";
+        if(local == visitor)
+        {
+            ans = getString(R.string.empate);
+        }
+        else if (local > visitor)
+        {
+            ans = getString(R.string.localWon);
+        }
+        else
+        {
+            ans = getString(R.string.visitanteWon);
+        }
+        return ans;
+
     }
 }
